86c6fccfe1834484c1653baa1b6f91d1766c02e0
mdYeasinIslam <hyeasinislam@gmail.com>
2025-02-20 22:27:28 +0600

    connect to the mongodb and create api for save users data

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.bfv30pl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const port = process.env?.PORT || 5000
 app.use(cors())
 app.use(express.json())
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      const userCollection =client.db('drag-nd-drop-task').collection('usersData')
+
+    //   const result = await userCollection.insertOne({ name: 'ali' })
+      //   res.send(result)
+      app.post('/usersInfo', async (req, res) => {
+          const body = req.body;
+          console.log(body)
+          const findUser = { email: body.email }
+          const findFromDB = await userCollection.findOne(findUser)
+          console.log(findFromDB)
+          if (findFromDB) {
+              return res.send('The user already in database')
+          }
+          const result = await userCollection.insertOne(body);
+          res.send(result)
+      })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('server is running properly')
