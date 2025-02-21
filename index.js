0e119038b3d2d8ff79f047edf55a80dacc4d2779
mdYeasinIslam <hyeasinislam@gmail.com>
2025-02-21 11:15:34 +0600

    api create for task category update and delete

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.bfv30pl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const port = process.env?.PORT || 5000
+
+
 app.use(cors())
 app.use(express.json())
 
@@ -21,11 +23,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
-      
-      const userCollection =client.db('drag-nd-drop-task').collection('usersData')
-
-    //   const result = await userCollection.insertOne({ name: 'ali' })
-      //   res.send(result)
+      const database = client.db('drag-nd-drop-task')
+      const userCollection =database.collection('usersData')
+      const taskCollection = database.collection('taskData')
+    
+    // Users Data
       app.post('/usersInfo', async (req, res) => {
           const body = req.body;
           console.log(body)
@@ -38,7 +40,37 @@ async function run() {
           const result = await userCollection.insertOne(body);
           res.send(result)
       })
-
+      
+    //Task data
+      app.post('/tasks', async (req, res) => {
+        const body = req.body;
+        console.log(body)
+        const result = await taskCollection.insertOne(body)
+        res.send(result)
+      })
+    app.get('/tasks', async (req, res) => {
+      const result = await taskCollection.find().toArray()
+      res.send(result)
+    })
+    app.put('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const find = { _id: new ObjectId(id) }
+      const findTask = await taskCollection.findOne(find);
+      const doc = {
+        $set: {
+          category: body.category
+        }
+      }
+      const result = await taskCollection.updateOne(find, doc)
+      res.send(result)
+    })
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const result = await taskCollection.deleteOne(filter)
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
